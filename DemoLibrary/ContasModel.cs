e39acaf4ea5b566f8fb4ee51b2e919919063cb9d
Rodrigo Mathias Mescua <rmathias@small>
2019-03-28 13:57:59 -0300

    Adicionar arquivos de projeto.

---
+++ DemoLibrary/ContasModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    class ContasModel
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public decimal Saldo { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return $"{ Id } - { Descricao }";
+            }
+        }
+    }
+}
